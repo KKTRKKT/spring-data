5398b0e16199caae6d47a07f6ce8c29e52df2cf0
KKTRKKT <paulhana6006@gmail.com>
2023-06-13 23:48:42 +0900

    엔티티 애노테이션 추가

--- src/main/java/me/kktrkkt/springdata/Account.java
+++ src/main/java/me/kktrkkt/springdata/Account.java
@@ -2,10 +2,8 @@ package me.kktrkkt.springdata;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 
 @Entity // 해당 객체를 테이블에 매핑하는 애노테이션
@@ -13,13 +11,20 @@ import javax.persistence.Id;
 public class Account {
 
     @Id // 식별자 애노테이션
-    @GeneratedValue // 값 자동 생성
+    @GeneratedValue // 값 자동 생성, DB에 따라
     private Long id;
 
     // @Column (생략) //변수명-컬럼명으로 자동 매핑됨
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
+
+    @Temporal(TemporalType.TIMESTAMP) // 날짜 컬럼을 매핑해준다
+    private Date created = new Date();
+
+    @Transient // 컬럼 매핑 대상에서 제외시킨다
+    private Boolean no;
 }
 
 
