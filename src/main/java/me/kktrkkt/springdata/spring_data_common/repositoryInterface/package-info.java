861a97028ad84ce831ce5fde7932f0acc231301b
KKTRKKT <paulhana6006@gmail.com>
2023-06-21 17:26:02 +0900

    Create package-info.java

---
+++ src/main/java/me/kktrkkt/springdata/spring_data_common/repositoryInterface/package-info.java
@@ -0,0 +1,6 @@
+package me.kktrkkt.springdata.spring_data_common.repositoryInterface;
+
+/*
+이 패키지에서는 @RepositoryDefinition를 이용해 기능을 일일히 정의하는 방법과
+@NoRepositoryBean을 이용해 커스텀한 repository 인터페이스를 만드는 방법에 대해 알아본다
+ */
