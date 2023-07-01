e451ea7f351594712722494cbb7cbf5d3cfd4f0c
KKTRKKT <paulhana6006@gmail.com>
2023-07-01 21:28:18 +0900

    Create package-info.java

---
+++ src/main/java/me/kktrkkt/springdata/spring_data_common/query_dsl/package-info.java
@@ -0,0 +1,10 @@
+package me.kktrkkt.springdata.spring_data_common.query_dsl;
+
+/*
+이 패키지에서는 queryDSL에 대해 알아본다.
+
+메소드로 쿼리를 생성하다보면 메소드가 길어져 보기가 안좋아진다.
+그래서 쿼리를 사용해야하는데 jpql은 타입 세이프하지 않고, criteria는 타입세이프하지만 코드가 단순해지지 않는다.
+따라서 타입 세이프하면서 단순하게 쿼리를 생성해주는 툴로 queryDSL이 사용된다.
+
+ */
